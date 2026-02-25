75e86cff95825fe39f018c7b760b9973befa7c67
marianluger55-oss <marianluger55-oss>
2026-02-25 15:20:25 +0100

    1

--- oneMaui/oneMaui/MainPage.xaml.cs
+++ oneMaui/oneMaui/MainPage.xaml.cs
@@ -2,23 +2,17 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            
+            lWelcome.Text = $"Herzlich Willkommen, {eName.Text}!";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
