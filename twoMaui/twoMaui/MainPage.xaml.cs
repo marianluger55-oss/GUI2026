8eb7be9242275ba660eb4d20d3dd25347041c08e
marianluger55-oss <marianluger55-oss>
2026-02-25 15:50:19 +0100

    123

---
+++ twoMaui/twoMaui/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using twoMaui;
+
+namespace twoMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string input = xnumber.Text;
+
+            int newinput = int.Parse(input); 
+
+           
+
+            if(newinput % 2 == 0)
+            {
+                ltext.Text = "Zahl ist gerade."; 
+            }
+            else
+            {
+                ltext.Text = "Zahl ist ungerade"; 
+            }
+        }
+    }
+}
