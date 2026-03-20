c4f979f5ed08d7cf63bce5f178fbbcdd358f07b8
marianluger55-oss <marianluger55-oss>
2026-03-20 10:56:05 +0100

    blublublu

--- twoMaui/FourtyControlesDemo/MainPage.xaml.cs
+++ twoMaui/FourtyControlesDemo/MainPage.xaml.cs
@@ -7,9 +7,18 @@
         public MainPage()
         {
             InitializeComponent();
+            lSwitch.Text = "eingeschaltet"; 
         }
 
-       
-        
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true) {
+                lSwitch.Text = "eingeschaltet"; 
+            }
+            else
+            {
+                lSwitch.Text = "ausgeschaltet"; 
+            }
+        }
     }
 }
