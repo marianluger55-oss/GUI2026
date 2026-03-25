d0612afe0972850e68a38a6ae9a00277c1a70e74
marianluger55-oss <marianluger55-oss>
2026-03-25 16:28:32 +0100

    edit

--- twoMaui/Event/MainPage.xaml.cs
+++ twoMaui/Event/MainPage.xaml.cs
@@ -9,6 +9,21 @@
             InitializeComponent();
         }
 
-       
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true)
+            {
+                lSwitch.Text = "Catering";
+            }
+            else
+            {
+                lSwitch.Text = "Kein Catering";
+            }
+        }
     }
 }
